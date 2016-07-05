b948bb4f9f277c3ed33f1209f0b09d206d6ccb03
leozhou <492753662@qq.com>
2016-07-06 00:36:33 +0800

    update for background to add some fields like highest score, score, length, speed

--- src/sprites/background/index.js
+++ src/sprites/background/index.js
@@ -43,6 +43,68 @@ var postion = function(i, j) {
 }
 initBlocks();
 
-//
+//最高分
+var titleStyle = {
+    font : 'bold italic 36px Arial',
+    fill : '#010600',
+    wordWrap : true,
+    wordWrapWidth : 440
+};
+
+var highestTitle = new PIXI.Text('最高分',titleStyle);
+highestTitle.x = 485;
+highestTitle.y = 20;
+
+backgroundCT.addChild(highestTitle);
+
+var highestScore = new PIXI.Text("0", titleStyle);
+highestScore.x = 500;
+highestScore.y = 65;
+backgroundCT.setHighestScore= function(score){
+    highestScore = new PIXI.Text(score, titleStyle);
+}
+backgroundCT.addChild(highestScore);
+
+//分数
+var scoreTitle = new PIXI.Text('分数', titleStyle);
+scoreTitle.x = 485;
+scoreTitle.y = 150;
+backgroundCT.addChild(scoreTitle);
+
+var score = new PIXI.Text("0", titleStyle);
+score.x = 500;
+score.y = 195;
+backgroundCT.setScore = function(score){
+    score = new PIXI.Text(score, titleStyle);
+}
+backgroundCT.addChild(score);
+
+//长度
+var lengthTitle = new PIXI.Text('长度', titleStyle);
+lengthTitle.x = 485;
+lengthTitle.y = 280;
+backgroundCT.addChild(lengthTitle);
+
+var length = new PIXI.Text("0", titleStyle);
+length.x = 500;
+length.y = 325;
+backgroundCT.setLength = function(length){
+    score = new PIXI.Text(length, titleStyle);
+}
+backgroundCT.addChild(length);
+
+//速度
+var speedTitle = new PIXI.Text('速度', titleStyle);
+speedTitle.x = 485;
+speedTitle.y = 410;
+backgroundCT.addChild(speedTitle);
+
+var speed = new PIXI.Text("0", titleStyle);
+speed.x = 500;
+speed.y = 455;
+backgroundCT.setSpeed = function(speed){
+    score = new PIXI.Text(speed, titleStyle);
+}
+backgroundCT.addChild(speed);
 
 module.exports = backgroundCT;
---
+++ src/sprites/numberGenerator.js
@@ -0,0 +1,28 @@
+/**
+ * Created by zhouchunjie on 16/7/5.
+ */
+var param = require('../params')
+var numGenerator = function(color, postion) {
+    var container = new PIXI.Container()
+    container.x = postion.x
+    container.y = postion.y
+    var rectangleBox = new PIXI.Graphics();
+    rectangleBox.lineStyle(2, color, 1);
+// draw a shape
+    rectangleBox.moveTo(0,0);
+    rectangleBox.lineTo(0, 44.5);
+    rectangleBox.lineTo(44.5, 44.5);
+    rectangleBox.lineTo(44.5, 0);
+    rectangleBox.lineTo(0, 0);
+    rectangleBox.endFill();
+    container.addChild(rectangleBox);
+    var graphics = new PIXI.Graphics()
+    graphics.beginFill(color, 1)
+    graphics.drawRect(4.5, 4.5, 35.5, 35.5)
+    graphics.endFill()
+    container.addChild(graphics)
+    //container.interactive = true
+    return container
+}
+
+module.exports = numGenerator;
